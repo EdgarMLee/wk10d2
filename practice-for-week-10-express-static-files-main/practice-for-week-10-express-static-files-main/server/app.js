7dfb8978016cf4790d17aa550e7c375ecd0f38db
edgar <edgarlee135@gmail.com>
2022-06-14 10:11:27 -0700

    5

--- practice-for-week-10-express-static-files-main/practice-for-week-10-express-static-files-main/server/app.js
+++ practice-for-week-10-express-static-files-main/practice-for-week-10-express-static-files-main/server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-// Your code here
+// app.use('/css/application.css', express.static('assets'));
+// app.use('/', express.static('/assets'));
+app.use('/scripts', express.static('assets/scripts'))
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
