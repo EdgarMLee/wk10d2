3d7fbd162d275874be3947b700b0287a11db6fa9
edgar <edgarlee135@gmail.com>
2022-06-14 13:20:52 -0700

    7

---
+++ notes/review.js
@@ -0,0 +1,12 @@
+// Creating Express Application
+const express = require('express')
+
+//Setting up a Router
+const router = express.Router();
+
+const contentCheck = (req, res, next) => {
+  const content = req.body.content;
+  if ()
+}
+
+module.exports = router
--- practice-for-week-10-express-router-main/practice-for-week-10-express-router-main/server/app.js
+++ practice-for-week-10-express-router-main/practice-for-week-10-express-router-main/server/app.js
@@ -4,5 +4,14 @@ const app = express();
 const peopleRouter = require('./routes/people');
 app.use('/people', peopleRouter);
 
+router.get('/', (req, res) => {
+  res.json('GET /colors')
+});
+
+router.get("/:name", (req, res) => {
+  res.json('GET /colors/:name')
+})
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
+
